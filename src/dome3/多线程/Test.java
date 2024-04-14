8376d62073bfbba7ac0d247991965ef9fc19529e
Yang ZL <969501572@qq.com>
2024-04-14 16:20:22 +0800

    4.14测试多线程

---
+++ src/dome3/多线程/Test.java
@@ -0,0 +1,13 @@
+package dome3.多线程;
+
+/**
+ * @author Zhong.Yl
+ * 2024/4/14 16:19
+ * java_xuexi
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("哈哈");
+
+    }
+}
